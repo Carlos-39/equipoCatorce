c01701625883c08f7564ddba15202cd56d028dde
Jose Manuel Palma Oquendo <80994908+JoseManuel2005@users.noreply.github.com>
2025-05-03 14:56:11 -0500

    feat: agregar ícono personalizado y animación de huella digital con Lottie

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,9 @@ android {
             )
         }
     }
+    buildFeatures {
+        dataBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -43,7 +46,10 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment)
+    //Lottie para imagenes animadas
+    implementation("com.airbnb.android:lottie:6.6.6")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
 }
