60a7337e40783e12d311ae7e48454b869dbf258b
Carlos Daniel Corrales <carlox59681@gmail.com>
2025-05-06 00:01:35 -0500

    hotfix de la respuesta de las API's

--- app/src/main/java/com/example/equipocatorce/model/DogBreedResponse.kt
+++ app/src/main/java/com/example/equipocatorce/model/DogBreedResponse.kt
@@ -5,6 +5,6 @@ import com.google.gson.annotations.SerializedName
 data class DogBreedResponse(
     @SerializedName("message")
     val message: Map<String, List<String>>,
-    @SerializedName("Status")
+    @SerializedName("status")
     val status: String
 )
