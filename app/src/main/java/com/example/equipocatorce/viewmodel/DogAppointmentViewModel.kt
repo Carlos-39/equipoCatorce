a752178c6996e809845061ff37f3bee69c55a751
Carlos Daniel Corrales <carlox59681@gmail.com>
2025-05-05 23:32:51 -0500

    view model completado

--- app/src/main/java/com/example/equipocatorce/viewmodel/DogAppointmentViewModel.kt
+++ app/src/main/java/com/example/equipocatorce/viewmodel/DogAppointmentViewModel.kt
@@ -4,8 +4,11 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.example.equipocatorce.model.DogAppointment
+import com.example.equipocatorce.model.DogImageResponse
 import com.example.equipocatorce.repository.DogAppointmentRepository
+import kotlinx.coroutines.launch
 
 class DogAppointmentViewModel(application: Application): AndroidViewModel(application) {
     val context = getApplication<Application>()
@@ -18,6 +21,87 @@ class DogAppointmentViewModel(application: Application): AndroidViewModel(applic
     private val _progresState = MutableLiveData(false)
     val progresState: LiveData<Boolean> = _progresState
 
-    // Almacenar lista de razas (TODO)
+    private val _dogBreedList = MutableLiveData<Map<String, List<String>>> ()
+    val dogBreedList: LiveData<Map<String, List<String>>> get() = _dogBreedList
 
+    private val _dogImage = MutableLiveData<DogImageResponse>()
+    val dogImage: LiveData<DogImageResponse> get() = _dogImage
+
+    fun saveAppointment(dogAppointment: DogAppointment) {
+        viewModelScope.launch {
+            _progresState.value = true
+
+            try {
+                dogAppointmentRepository.saveAppointment(dogAppointment)
+                _progresState.value = false
+            } catch (e:Exception) {
+                _progresState.value = false
+            }
+        }
+    }
+
+    fun getAppointments() {
+        viewModelScope.launch {
+            _progresState.value = true
+
+            try {
+                _dogAppointments.value = dogAppointmentRepository.getAppointments()
+                _progresState.value = false
+            } catch (e:Exception) {
+                _progresState.value = false
+            }
+        }
+    }
+
+    fun deleteAppointment(dogAppointment: DogAppointment) {
+        viewModelScope.launch {
+            _progresState.value = true
+
+            try {
+                dogAppointmentRepository.deleteAppointment(dogAppointment)
+                _progresState.value = false
+            } catch (e:Exception) {
+                _progresState.value = false
+            }
+        }
+    }
+
+    fun updateAppointment(dogAppointment: DogAppointment) {
+        viewModelScope.launch {
+            _progresState.value = true
+
+            try {
+                dogAppointmentRepository.updateAppointment(dogAppointment)
+                _progresState.value = false
+            } catch (e:Exception) {
+                _progresState.value = false
+            }
+        }
+    }
+
+    fun getBreeds() {
+        viewModelScope.launch {
+            _progresState.value = true
+
+            try {
+                _dogBreedList.value = dogAppointmentRepository.getBreeds()
+                _progresState.value = false
+            } catch (e:Exception) {
+                _progresState.value = false
+            }
+        }
+    }
+
+    fun getImage() {
+        viewModelScope.launch {
+            _progresState.value = true
+
+            try {
+                _dogImage.value = dogAppointmentRepository.getImage()
+                _progresState.value = false
+            } catch (e:Exception) {
+                _progresState.value = false
+            }
+        }
+    }
 }
