027c836d57576134f61c54e755753aab552efbf7
laura <laura.bueno@correounivalle.edu.co>
2025-05-08 20:53:39 -0500

    Al presionar en el botón Guardar Cita, se agregó el mensaje emergente con la frase de Selecciona un síntoma (Trabajando con Camilo)

--- app/src/main/java/com/example/equipocatorce/network/DogApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.equipocatorce.network
-
-import retrofit2.Call
-import retrofit2.http.GET
-
-interface DogApiService {
-    @GET("breeds/list/all")
-    fun getAllBreeds(): Call<DogBreedsResponse>
-}
